6d75f5f2220a5973ead5929f3f8c8d10621b26fb
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-05-01 19:15:34 +0100

    4th commit

--- search.js
+++ search.js
@@ -8,7 +8,30 @@ function searchFunction() {
     alert('You searched for' + " " + searchQuery);
 }
 
+function fetchResults() {
+    var Airtable = require('airtable');
+    var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appmaFyHEvGhBLhuV');
 
+    base('Table 1').select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 3,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function (record) {
+            console.log('Retrieved', record.get('Name'));
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    });
+}
 //search query
 
 /*
