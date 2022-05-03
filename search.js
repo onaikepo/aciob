31392e4c7961265e2ab1d8b00af2defd92f589be
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-05-03 20:26:28 +0100

    search.js updated with API intergration

--- search.js
+++ search.js
@@ -1,37 +1,79 @@
 //search.js
 
-const searchListenser = document.getElementById("search-submit");
-searchListenser.addEventListener("click", searchFunction);
+/*var mockArray = [
+    ['Bitcoin', 'Cryptocurrency', 'Bitcoin is a projec thtat is able to transfer peer to peer payments through a global distrubuted ledger.']
+    ['aaaaaaa', '11111111111', 'Lorem Ipsum this a test of the data to see if connection works.']
+    ['bbbbbbbb', '2222222222', 'Lorem ipsum this will be interesting']
+    ['cccccccc', '3333333333', 'This is a good tv show about coindg and hacking.']
+    ['dddddddd', '44444444444', 'I wonder how long I can make this sentence to test this functionality']
+    ['eeeeeeeee', '5555555555', 'This is a really long way to test if this javascript code block will render the HTML correctly']
+    ['fffffffff', '6666666666', 'This a really exciting project and I hope this is going to go further.']
+    ['ggggggggg', '7777777777', 'This si a really fun Javascript project and javascript codebase.']
+    ['hhhhhhhhhh', '8888888888', 'Cryptocurrency is the revolution and is the next phase of the human frontier of technology']
+]*/
 
-function searchFunction() {
-    var searchQuery = document.getElementById("query").value;
-    alert('You searched for' + " " + searchQuery);
+//This connects to the API and fetches all of the data from the records. 
+
+
+var responseData = ''
+
+function indexerFunction() {
+
+    function fetchData() {
+        var url = "https://api.airtable.com/v0/appmaFyHEvGhBLhuV/tbliErYubz0325AdX?fields%5B%5D=_reponame&fields%5B%5D=_topics&fields%5B%5D=_description&fields%5B%5D=_homepage";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+
+        xhr.setRequestHeader("Authorization", process.env.airtable_bearer_key);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+                alert(xhr.responseText);
+            }
+        };
+
+        xhr.send();
+
+        var responseData = xhr.responseText;
+    }
+
+    function displayResults() {
+        //expects and array. 
+        //for each item in the array create a new <div> 
+
+        for (let i = 0; i < resultCount; i++) {
+            // Runs the length of the result array. 
+            console.log('Walking east one step');
+            const node = document.createElement("div");
+            const nameNode = document.createElement("h2");
+            const descNode = document.createElement("p");
+            const urlString = document.createElement('p');
+        }
+
+    }
+
+    const searchListenser = document.getElementById("search-submit");
+    searchListenser.addEventListener("click", searchFunction);
+
+    function searchFunction() {
+        var searchQuery = document.getElementById("query").value;
+        alert('You searched for' + " " + searchQuery);
+        indexerFunction();
+    }
+
+    var resultArray = []
+    //result array will list each of the individual items in the array. 
+
+    var resultCount = 0
+    //result count should take the number frm the API for the amount of records which match the formula. 
+
+
 }
 
-function fetchResults() {
-    var Airtable = require('airtable');
-    var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appmaFyHEvGhBLhuV');
 
-    base('Table 1').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 3,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-
-        records.forEach(function (record) {
-            console.log('Retrieved', record.get('Name'));
-        });
-
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
-
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
-}
 //search query
 
 /*
@@ -61,3 +103,28 @@ event.preventDefault()
 function 
 
 */
+
+/*function fetchResults() {
+        var Airtable = require('airtable');
+        var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appmaFyHEvGhBLhuV');
+
+        base('Table 1').select({
+            // Selecting the first 3 records in Grid view:
+            maxRecords: 3,
+            view: "Grid view"
+        }).eachPage(function page(records, fetchNextPage) {
+            // This function (`page`) will get called for each page of records.
+
+            records.forEach(function (record) {
+                console.log('Retrieved', record.get('Name'));
+            });
+
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            fetchNextPage();
+
+        }, function done(err) {
+            if (err) { console.error(err); return; }
+        });
+    }*/
