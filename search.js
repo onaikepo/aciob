23ddd3620710ec2dd1c8c947a5f491414def0e3d
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-05-18 21:51:38 +0100

    updates to search.js to list the array of records.

--- search.js
+++ search.js
@@ -14,12 +14,15 @@
 
 //This connects to the API and fetches all of the data from the records. 
 
-var resultArray = []
+//var resultArray = []
 //result array will list each of the individual items in the array. 
 
 var resultCount = 0
 //result count should take the number frm the API for the amount of records which match the formula.
 
+var searchResultItem = ""
+
+var searchRecordsIndexCount = 0;
 
 function searchTitle() {
 
@@ -29,47 +32,145 @@ function searchTitle() {
     searchResultTitle.innerHTML = searchHeader;
 }
 
+
+
 var responseData = ''
 
 function fetchData() {
+
     var url = "https://api.airtable.com/v0/appmaFyHEvGhBLhuV/tbliErYubz0325AdX?fields%5B%5D=_reponame&fields%5B%5D=_topics&fields%5B%5D=_description&fields%5B%5D=_homepage";
 
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
 
-    xhr.setRequestHeader("Authorization", process.env.airtable_bearer_key);
+    xhr.setRequestHeader("Authorization", "process.env.AIRTABLE_API_KEY);
 
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-            alert(xhr.responseText);
-        }
-    };
+            if (xhr.readyState === 4) {
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+                //alert(xhr.responseText);
+
+                var responseData = [xhr.responseText];
+
+                var searchResultItem = JSON.parse(responseData);
+                //This variable is an object
+
+
+                var resultArray = [];
+                //alert(JSON.stringify(searchResultItemCount));
+
+                var searchCount = resultArray.push(searchResultItem);
+
+                listResults();
+
+                //var searchResultRecords = JSON.stringify(searchResultItem["records"][0]);
+
+                //var searchFilter = resultArray.find(results => resultArray.fields._reponame === "bitcoin");
+
+                //document.getElementById("searchItem").innerHTML = JSON.stringify(searchResultRecords);
+
+
+                for (i = 0; i < resultArray.length; i++) {
+                    var c = resultArray[i];
+                    //alert(resultArray.length);   
+                }
+            }
+        };
 
     xhr.send();
-
-    var responseData = xhr.responseText;
 }
+
+
+function searchData() {
+
+    var searchingQuery = document.getElementById("query").value;
+
+    var matchQuery = JSON.stringify(searchResultItem["records"][searchRecordsIndexCount].fields);
+
+    if (searchingQuery) {
+        if (matchQuery.includes(searchingQuery)) {
+            alert("the term is in the array");
+            //return all of the indexes that this search term appears. We could add in a for each loop to if statement and keep checking it. 
+        }
+    } else {
+        alert('Please enter a search query.');
+    };
+
+
+    var searchArray = searchResultItem.find(_reponame => _reponame === searchQuery);
+
+    alert(searchArray);
+    //We will need to search more than the product name it will need find on all of the fields e.g.description, reponame and topics.
+
+    //return the fields that match as a new array of Indicies then we can pass each of those arrays into the for each loop. 
+    //So then the loop should only continue with the list of terms that have valid strings within the array. 
+}
+
+function listResults() {
+    for (i = 0; i < 100; i++) {
+
+        const e = document.createElement('div');
+        e.classList.add('container');
+        var indexCount = JSON.stringify(searchResultItem["records"][searchRecordsIndexCount].fields._reponame);
+
+        var a = document.createElement('a');
+        var resultHomepage = JSON.stringify(searchResultItem["records"][searchRecordsIndexCount].fields._homepage);
+
+        var b = document.createElement('p');
+        var resultDescription = JSON.stringify(searchResultItem["records"][searchRecordsIndexCount].fields._description);
+
+        var linebreak = '<br>';
+
+        const f = document.createElement('div')
+        f.classList.add('container');
+
+
+        e.innerHTML = "Project Name:" + indexCount.replaceAll(/['"]+/g, '') + linebreak + "Project Description: " + resultDescription + linebreak;
+        f.innerHTML = "Project URL:" + resultHomepage;
+        //document.body.appendChild(indexCountBreak);
+        document.body.appendChild(a);
+        document.body.appendChild(b);
+        document.body.appendChild(e);
+        document.body.appendChild(f);
+
+
+        searchRecordsIndexCount++;
+    }
+}
+
+/* if (searchResultItem === String) {
+    alert('This is a string')
+}   else if (searchResultItem === Array) {
+    alert('This is an array.')
+} else {
+    alert('This variable is a' + ' ' + (typeof searchCount))
+}*/
+
+
 
 const searchListenser = document.getElementById("search-submit");
 searchListenser.addEventListener("click", searchFunction);
 
 function searchFunction() {
     var searchQuery = document.getElementById("query").value;
-    alert('You searched for' + " " + searchQuery);
+    ///alert('You searched for' + " " + searchQuery);
     searchTitle();
     fetchData();
+    searchData();
 }
 
 
+
 function displayResults() {
+    //indexer should be able to fliter() through all of the available results from the JSON araay. 
+    //once those results are filtered then they would need to be passed across to the front end and for each individual records in the filter we would need to pass a <h2> Project Title.
 
-    JSON.parse(responseData)
+    //document.getElementById("searchItem").innerHTML = responseData;
 
+    /* JSON.parse(responseData)
     var searchResult = document.getElementById("search-result");
-
-    searchResult.innerHTML = responseData;
+    searchResult.innerHTML = responseData;*/
 
     //expects and array. 
     //for each item in the array create a new <div> 
